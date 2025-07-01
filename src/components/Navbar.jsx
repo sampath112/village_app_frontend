8ef64cafbe3d924223807da25c0472ebcb1f395b
sampath chowdary <96104961+sampath112@users.noreply.github.com>
2025-07-01 16:32:59 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
     <nav className="bg-white text-text fixed w-full top-0 z-50 shadow-md">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
         <Link to="/" className="text-2xl sm:text-3xl font-bold text-primary">
-          Mana Chinnabondapalli
+          Mana Chinabondapalli
         </Link>
 
         {/* Desktop Menu */}
