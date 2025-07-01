432dc75c41c593b0354a60e6948ee045bfee1aa7
sampath chowdary <96104961+sampath112@users.noreply.github.com>
2025-07-01 14:46:10 +0530

    Update ProfileCard.jsx

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -51,12 +51,7 @@ function ProfileCard({ profile, onDelete }) {
           </span>
         </p>
       </div>
-      <button
-        onClick={() => onDelete(profile._id)}
-        className="mt-4 bg-red-600 text-white px-4 py-2 rounded-lg hover:bg-red-700 transition-all"
-      >
-        Delete Profile
-      </button>
+      
     </div>
   );
 }
